da5275cd6b7d3f90eddc1bcf87207bdd3285f7a9
lucas3768 <119453950+lucas3768@users.noreply.github.com>
2023-07-04 18:54:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var img ="" 
+function preload() {
+
+img = loadImage('dog_cat.jpg')
+
+}
+function setup() {
+canvas = createCanvas(640,420)
+canvas.center()
+}
+function draw() {
+    image(img,0,0,640,420)
+rect(0,0,639,419)
+}

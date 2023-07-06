a3c8e643f910c81acb530076bd37141218de9052
lucas3768 <119453950+lucas3768@users.noreply.github.com>
2023-07-06 17:47:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var img ="" 
+var model_status = ""
 function preload() {
 
 img = loadImage('dog_cat.jpg')
@@ -7,8 +8,22 @@ img = loadImage('dog_cat.jpg')
 function setup() {
 canvas = createCanvas(640,420)
 canvas.center()
+Detectordeobjetos = ml5.objectDetector("cocossd",modelloaded)
+}
+function modelloaded() {
+    console.log("modelo carregado")
+    model_status = true
+    Detectordeobjetos.detect(img,gotresults)
+}
+function gotresults(error,results) {
+    if(error)
+    console.log(error)
+    else
+    console.log(results)
 }
 function draw() {
-    image(img,0,0,640,420)
-rect(0,0,639,419)
+    image(img,0,0,640,420);
+    noFill();
+rect(60,50,300,200);
+text("cachorro",70,60);
 }
